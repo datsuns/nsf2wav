9e47237c1949e941cfd9608e6840a0d865cd0e70
datsuns <the.skeleton7@gmail.com>
2020-12-20 16:54:03 +0900

    fix download path

--- download.bat
+++ download.bat
@@ -1,7 +1,7 @@
 set GME_VER=0.5.2
 set DESTDIR=gem
 
-set URL="http://blargg.parodius.com/libs/Game_Music_Emu-%GME_VER%.zip"
+set URL="http://blargg.8bitalley.com/parodius/libs/Game_Music_Emu-%GME_VER%.zip"
 set DEST=.\%DESTDIR%\gem.zip
 
 rmdir /S /Q %DESTDIR%
